cb5a42f1097862583e16059dbb895398f4e04bd3
Pablo Gutierrez Diaz <pgutierrezd@ids.com.mx>
2019-07-17 12:15:48 -0500

    Cache file implementation is deleted.

--- app/src/main/java/com/example/basearchitecture/di/modules/data/NetworkModule.kt
+++ app/src/main/java/com/example/basearchitecture/di/modules/data/NetworkModule.kt
@@ -10,13 +10,10 @@ import com.example.basearchitecture.data.network.interfaces.INetwork
 import com.example.basearchitecture.ui.app.config.ReadFileConfig
 import dagger.Module
 import dagger.Provides
-import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
-import java.io.File
 import java.util.concurrent.TimeUnit
-
 
 /**
  * NetworkModule
@@ -38,28 +35,6 @@ class NetworkModule {
     private val CONECTION_TIME_OUT: Long = 60
 
     /**
-     * Method to provide cache file
-     *
-     * @param context context instance
-     * @return cache file
-     */
-    @Provides
-    fun cacheFile(context: Context): File {
-        return File(context.cacheDir, "okhttp_cache")
-    }
-
-    /**
-     * Method to provide cache
-     *
-     * @param cacheFile cache file
-     * @return cache instance
-     */
-    @Provides
-    fun cache(cacheFile: File): Cache {
-        return Cache(cacheFile, 10 * 1024 * 1024)
-    }
-
-    /**
      * Method to init interceptor
      *
      * @return http logging interceptor
@@ -74,18 +49,19 @@ class NetworkModule {
     /**
      * Method to provide okhttp client
      *
-     * @param cache cache instance
+     * @param loggingInterceptor logging interceptor instance
+     *
      * @return okhttp client
      */
     @Provides
-    fun okHttpClient(cache: Cache, loggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
+    fun okHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
         loggingInterceptor.level =
                 if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .readTimeout(READ_TIME_OUT, TimeUnit.SECONDS)
             .connectTimeout(CONECTION_TIME_OUT, TimeUnit.SECONDS)
-            .cache(cache)
     }
 
     /**
